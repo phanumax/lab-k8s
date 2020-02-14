d18be3540df55da5ad5182479c3754de96fdf5f5
phanumax@gmail.com <phanumax@gmail.com>
2020-02-14 11:00:00 +0700

    20200214

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const os = require('os')
 
-console.log('Kubia-dev server starting...')
+console.log('Kubia server starting...')
 
 var handler = function(req, res) {
   console.log('Received request from ' + req.connection.remoteAddress)
